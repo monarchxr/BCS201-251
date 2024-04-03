8b9586b43d986b4363a7a70f14b37ee77a0ccdf5
Raunak Jais <90858989+monarchxr@users.noreply.github.com>
2024-04-03 16:29:01 +0530

    Add files via upload

---
+++ Lab1/L1Q1.c
@@ -0,0 +1,20 @@
+//LAB - 1 QUESTION 1
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+    
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    
+    int N1,N2;
+    scanf("%d %d", &N1, &N2);
+    N1 = N1 + N2;
+    N2 = N1 - N2;
+    N1 = N1 - N2;
+    printf("%d %d", N1,N2);
+    
+    return 0;
+}
---
+++ Lab1/L1Q3.c
@@ -0,0 +1,16 @@
+//LAB 1 QUESTION 3
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */  
+    
+    float temp;
+    scanf("%f", &temp);
+    printf("%.2f\n", ((temp-32)*(5))/9);
+    return 0;
+}
