ace7b69170f47818f832d0e54e2b36f7befec008
Raunak Jais <90858989+monarchxr@users.noreply.github.com>
2024-05-02 15:36:57 +0530

    Create L6Q1.c

---
+++ Lab6/L6Q1.c
@@ -0,0 +1,36 @@
+//program to print armstrong number in a range
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main(){
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    int a,b,n;
+    scanf("%d %d", &a, &b);
+    for(n=a; n<b; n++){
+        int temp = n;
+        int count = 0;
+        int digit = 0, digitpsum = 0;
+
+        while(temp!=0){
+            temp/=10;
+            count++;
+        }
+
+        temp = n;
+
+        while(temp!=0){
+            digit = temp%10;
+            digitpsum+= pow(digit,count);
+            temp/=10;
+        }
+
+        if(digitpsum==n){
+            printf("%d ", n);
+        }
+    }
+    return 0;
+}
