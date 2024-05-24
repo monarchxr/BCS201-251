7277a1dcf29cb65ebb4ee286b0033b05db5002b5
Raunak Jais <90858989+monarchxr@users.noreply.github.com>
2024-05-24 12:26:18 +0530

    Update L9Q3.c

--- Lab9/L9Q3.c
+++ Lab9/L9Q3.c
@@ -1,6 +1,4 @@
 //program for divide number into sum of 2 primes
-//not passed all test cases
-
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -22,7 +20,7 @@ int primecheck(int n){
 int main() {
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
-    int n;
+    int n,flag=0;
     scanf("%d", &n);
     if(n==1){
         return 0;
@@ -30,7 +28,12 @@ int main() {
     for(int i=2; i<=n/2; i++){
         if(primecheck(i) && primecheck(n-i)){
             printf("%d=%d+%d\n", n,i,n-i);
+            flag = 1;
         }
+    }
+    
+    if(!flag){
+        printf("NOT POSSIBLE");
     }
     return 0;
 }
